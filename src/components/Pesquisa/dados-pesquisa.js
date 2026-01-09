17314fa46019562687fd7a191183a9e898f40516
Patrick Farias Lima <patrickfarias@live.com>
2026-01-09 18:26:14 -0300

    feature: trabalhando com estados

---
+++ src/components/Pesquisa/dados-pesquisa.js
@@ -0,0 +1,22 @@
+import livro from "../../assets/images/livro.png"
+
+export const livros = [
+    {
+        nome: 'Liderança em Design', id: 1, src:livro,
+    },
+    {
+        nome: 'Javascript Assertivo', id: 2, src:livro,
+    },
+    {
+        nome: 'Aventureiros do Java', id: 3, src:livro,
+    },
+    {
+        nome: 'Design Thinking', id: 4, src:livro,
+    },
+    {
+        nome: 'Metodologias Agile', id: 5, src:livro,
+    },
+    {
+        nome: 'Liderança Agile', id: 6, src:livro,
+    },
+];
